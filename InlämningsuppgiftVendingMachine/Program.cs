2171db198ea73afeb09b6ba2e2e4be11f14a2101
BojanMirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2022-10-13 14:13:05 +0200

    Add project files.

---
+++ InlämningsuppgiftVendingMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InlämningsuppgiftVendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
